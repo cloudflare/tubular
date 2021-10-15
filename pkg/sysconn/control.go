bc48ceff7b79b9270c8184509a167706605dc188
Lorenz Bauer <lmb@cloudflare.com>
2021-10-15 12:19:10 +0100

    sysconn: name function arguments
    This allows editors like VSCode to autocomplete sensible closures.

--- pkg/sysconn/control.go
+++ pkg/sysconn/control.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Control invokes conn.SyscallConn().Control.
-func Control(conn syscall.Conn, fn func(int) error) error {
+func Control(conn syscall.Conn, fn func(fd int) error) error {
 	raw, err := conn.SyscallConn()
 	if err != nil {
 		return fmt.Errorf("SyscallConn: %w", err)
@@ -23,7 +23,7 @@ func Control(conn syscall.Conn, fn func(int) error) error {
 }
 
 // ControlInt invokes conn.SyscallConn().Control and returns an integer.
-func ControlInt(conn syscall.Conn, fn func(int) (int, error)) (int, error) {
+func ControlInt(conn syscall.Conn, fn func(fd int) (int, error)) (int, error) {
 	var value int
 	return value, Control(conn, func(fd int) (err error) {
 		value, err = fn(fd)
